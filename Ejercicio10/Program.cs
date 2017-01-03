453fff92766132da0ad2a3f1f252607a34477396
StevenBonilla <alexsteven360@gmail.com>
2017-01-03 11:05:42 +0100

    Ejercicio10

--- Ejercicio10/Program.cs
+++ Ejercicio10/Program.cs
@@ -10,6 +10,61 @@ namespace Ejercicio10
     {
         static void Main(string[] args)
         {
+            int[,] matrix;
+            string size;
+            int sizeInt;
+            string valor;
+            int valorInt;
+
+
+            Console.WriteLine("Introduzca el tamaño de la matriz (regular)"); //Tamaño de la matriz
+            size = Console.ReadLine();
+            Int32.TryParse(size, out sizeInt);
+
+            matrix = new int[sizeInt, sizeInt];
+
+            Console.WriteLine("  Introduzca los valores de la matrix:"); //Introducimos valores para cada matrix[fila,columna]
+            for (int i = 0; i < sizeInt; i++)
+            {
+                for (int j=0; j < sizeInt; j++)
+                {
+                    Console.WriteLine("Linea " + i + ", Columna " + j);
+                    valor = Console.ReadLine();
+                    Int32.TryParse(valor, out valorInt);
+                    matrix[i, j] = valorInt;
+                }
+            }
+
+            Console.WriteLine("\n  The matrix is: \n");
+
+
+            for (int i=0; i < sizeInt; i++)  //Imprimimos por pantalla la matriz
+            {
+                for (int j = 0; j < sizeInt; j++)
+                {
+                    Console.Write(" " + matrix[i,j]);
+                }
+                Console.WriteLine("");
+            }
+
+            int suma = 0; //Inicializamos el valor "suma" a 0
+            int[] diagonal = new int[sizeInt];
+
+            for (int i = 0; i < sizeInt; i++) 
+            {
+                suma = suma + matrix[i, i]; //vamos sumando cada valor de la diagonal matrix[0,0] + matrix[1,1] + matrix[2,2] 
+                diagonal[i] = matrix[i, i];
+            }
+
+            Console.WriteLine("\nAddition of the Main diagonal element is: " + suma);
+            Console.WriteLine("\nElements in array (diagonal) are: ");
+
+            for (int i = 0; i < sizeInt; i++)
+            {
+                Console.Write(" " + matrix[i,i]);
+            }
+
+            Console.ReadLine();
 
         }
     }
