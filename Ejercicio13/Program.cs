b74715b536a32edc98f7f7df1322e89f4756263a
StevenBonilla <alexsteven360@gmail.com>
2017-01-04 08:21:05 +0100

    Ejercicio 13. Area con métodos

---
+++ Ejercicio13/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio13
+{
+    class Program
+    {
+        double resultado;
+        double pi = Math.PI;
+
+        public double perimeter(double value)
+        {
+            resultado = 2 * pi * value;
+            return resultado;
+        }
+
+        public double area(double value)
+        {
+            resultado = pi * value * value;
+            return resultado;
+        }
+
+        static void Main(string[] args)
+        {
+            string radius;
+            double radiusDouble;
+            double perimeter;
+            double area;
+
+            Program circle = new Program(); 
+
+            Console.WriteLine("Input the radius of the circle: ");
+            radius = Console.ReadLine();
+            double.TryParse(radius, out radiusDouble);
+
+            perimeter = circle.perimeter(radiusDouble);
+            area = circle.area(radiusDouble);
+
+            Console.WriteLine("\n PERIMETER: " + perimeter);
+            Console.WriteLine("\n AREA: " + area);
+
+            Console.ReadLine();
+
+        }
+    }
+}
